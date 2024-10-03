3633976bf088ab80afcdb2be517481a16612304c
Suresh Babu <sureshbabu.kanini@gmail.com>
2024-10-03 16:41:28 +0530

    added role based dropdown - home-away

--- home-away/components/navbar/SignOutLink.tsx
+++ home-away/components/navbar/SignOutLink.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { SignOutButton } from "@clerk/nextjs";
+import React from "react";
 
 function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: "You have been signed out" });
+  };
   return (
-    <div>SignOutLink</div>
-  )
+    <SignOutButton redirectUrl="/">
+      <button className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
 
-export default SignOutLink
+export default SignOutLink;
