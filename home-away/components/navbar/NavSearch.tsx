39293ecd2b27b80821dd7b789cf85ce958ae82b1
Suresh Babu <sureshbabu.kanini@gmail.com>
2024-10-03 15:20:22 +0530

    Added Dark theme and customised navbar - home-away

--- home-away/components/navbar/NavSearch.tsx
+++ home-away/components/navbar/NavSearch.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Input } from "../ui/input";
 
 function NavSearch() {
   return (
-    <div>NavSearch</div>
-  )
+    <Input
+      type="text"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted"
+    ></Input>
+  );
 }
 
-export default NavSearch
+export default NavSearch;
